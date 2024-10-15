b2ffbf0ab4683f61f8dcecd13ca981ee3512a082
manikantaprasad212 <manikantaprasadprasadula@gmail.com>
2024-10-15 15:26:26 +0530

    Add files via upload

---
+++ Stash Reset Branches/git/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Best Animes</h1>
+     <h2>One Piece</h2>
+     <img src="./DemonSlayer/Luffy.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
